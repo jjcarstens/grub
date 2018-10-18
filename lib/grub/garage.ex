895e82548c3c94cca86b9407c7e7451f62ed4f81
Jon Carstens <jjcarstens@me.com>
2018-10-17 20:36:43 -0600

    add basic support for managing garage doors

---
+++ lib/grub/garage.ex
@@ -0,0 +1,12 @@
+defmodule Grub.GarageDoor do
+  alias ElixirALE.GPIO
+
+  defstruct id: nil, sensor: nil, opener: nil
+  
+  def create(id, opener_gpio, sensor_gpio) do
+    {:ok, opener} = GPIO.start_link(opener_gpio, :output)
+    {:ok, sensor} = GPIO.start_link(sensor_gpio, :input)
+
+    %__MODULE__{id: id, opener: opener, sensor: sensor}
+  end
+end
